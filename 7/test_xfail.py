8213d96e72efdb12f971412454974affdc4ec906
wux <amazinglala@163.com>
2020-04-21 17:23:09 +0800

    提交4.21日新增代码1

---
+++ 7/test_xfail.py
@@ -0,0 +1,24 @@
+#coding:utf-8
+
+import pytest
+
+class Test_Pytest():
+
+    @pytest.mark.xfail
+    def test_one(self):
+        print("test_one方法执行")
+        assert 1==2
+
+
+    def test_two(self):
+        print("test_two方法执行")
+        assert "o" in "love"
+
+
+    def test_three(self):
+        print("test_three方法执行")
+        assert 3-2==1
+
+
+if __name__ == "__main__":
+    pytest.main(["-s","test_xfail.py"])
